1c7d265d210c6887fc4af3765a3eb0cfab10ed78
Sourcery AI <>
2023-10-21 10:26:04 +0000

    'Refactored by Sourcery'

--- tests/typing/object.py
+++ tests/typing/object.py
@@ -4,7 +4,7 @@ from dependency_injector import providers
 
 
 # Test 1: to check the return type
-provider1 = providers.Object(int(3))
+provider1 = providers.Object(3)
 var1: int = provider1()
 
 # Test 2: to check the provided instance interface
@@ -12,7 +12,7 @@ provider2 = providers.Object(int)
 provided2: Type[int] = provider2.provided
 
 # Test 3: to check the return type with await
-provider3 = providers.Object(int(3))
+provider3 = providers.Object(3)
 async def _async3() -> None:
     var1: int = await provider3()  # type: ignore
     var2: int = await provider3.async_()
